561570c599a0c5d28311df8f8de57e738938cdf3
Ivanna Cantelberg <ivannacantelberg@gmail.com>
2023-12-13 15:19:33 +0100

    Fixed application files permissions

--- laravel-app/config/app.php
+++ laravel-app/config/app.php
@@ -168,9 +168,6 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-        App\Providers\FortifyServiceProvider::class,
-        App\Providers\JetstreamServiceProvider::class,
-        App\Providers\VoltServiceProvider::class,
     ])->toArray(),
 
     /*
